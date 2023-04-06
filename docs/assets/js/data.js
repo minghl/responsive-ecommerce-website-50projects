c2cf3bf6e92e6d7dcc87a34a01b451096931f8df
minghl <a879307253@gmail.com>
2023-04-06 13:44:05 +0800

    pagination

---
+++ docs/assets/js/data.js
@@ -0,0 +1,106 @@
+const data = [
+    {
+        className: 'bx-card',
+        title: 'Expanding Cards',
+        number: '01',
+        href: './projects/01Expanding Cards/index.html'
+    },
+    {
+        className: 'bx-skip-next-circle',
+        title: 'Progress Steps',
+        number: '02',
+        href: './projects/02Progress Steps/index.html'
+    },
+    {
+        className: 'bx-rotate-left',
+        title: 'Rotate Navigate',
+        number: '03',
+        href: './projects/03Rotating Navigation/index.html'
+    },
+    {
+        className: 'bx-search',
+        title: 'Hidden Search',
+        number: '04',
+        href: './projects/04Hidden Searcn/index.html'
+    },
+    {
+        className: 'bx-loader-circle',
+        title: 'Blurry Loading',
+        number: '05',
+        href: './projects/05Blurry Loading/index.html'
+    },
+    {
+        className: 'bx-align-middle',
+        title: 'Scroll Animation',
+        number: '06',
+        href: './projects/06Scroll Animation/index.html'
+    },
+    {
+        className: 'bx-dock-right',
+        title: 'Split Landing',
+        number: '07',
+        href: './projects/07Split Landing Page/index.html'
+    },
+    {
+        className: 'bx-text',
+        title: 'Form Wave',
+        number: '08',
+        href: './projects/08Form Wave/index.html'
+    },
+    {
+        className: 'bx-card',
+        title: 'Sound Board',
+        number: '09',
+        href: './projects/09Sound Board/index.html'
+    },
+    {
+        className: 'bx-laugh',
+        title: 'Dad Jokes',
+        number: '10',
+        href: './projects/10Dad Jokes/index.html'
+    },
+    {
+        className: 'bxs-keyboard',
+        title: 'Event KeyCodes',
+        number: '11',
+        href: './projects/11Event KeyCodes/index.html'
+    },
+    {
+        className: 'bx-question-mark',
+        title: 'Faq Collapse',
+        number: '12',
+        href: './projects/12Faq Collapse/index.html'
+    },
+    {
+        className: 'bx-dice-3',
+        title: 'Random Choices',
+        number: '13',
+        href: './projects/13Random Choice Picker/index.html'
+    },
+    {
+        className: 'bx-navigation',
+        title: 'Animated Nav',
+        number: '14',
+        href: './projects/14Animated Navigation/index.html'
+    },
+    {
+        className: 'bx-add-to-queue',
+        title: 'Incre Counter',
+        number: '15',
+        href: './projects/15 Incrementing Counter/index.html'
+    },
+    {
+        className: 'bx-drink',
+        title: 'Drink Water',
+        number: '16',
+        href: './projects/16 Drink Water/index.html'
+    },
+    {
+        className: 'bx-camera-movie',
+        title: 'Movie App',
+        number: '17',
+        href: './projects/17Movie App/index.html'
+    },
+];
+
+export default data;
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,3 +1,4 @@
+import data from './data.js';
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
@@ -22,8 +23,41 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
-/*===== CHANGE COLOR HEADER =====*/
-window.onscroll = () => {
-    const nav = document.getElementById('header');
-    if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
-};
+/*===== RENDER PROJECTS =====*/
+const featuredCon = document.querySelector('.featured__container');
+const pages = document.querySelector('.sneaker__pages')
+const projects = [];
+
+console.log(featuredCon, 'fc');
+console.log(pages, 'pages');
+
+data.forEach(project => {
+    projects.push(`
+    <article class="sneaker">
+    <i class="bx ${project.className} sneaker__img"></i>
+    <span class="sneaker__name">${project.title}</span>
+    <span class="sneaker__preci">${project.number}</span>
+    <a
+      href="${project.href}"
+      class="button-light sneaker__button"
+      >Click for Details
+      <i class="bx bx-right-arrow-alt button-icon"></i
+    ></a>
+  </article>
+    `);
+    featuredCon.innerHTML = projects.slice(0, 12).join('');
+})
+
+pages.addEventListener('click', e => {
+    e.preventDefault();
+    const projectStart = (+e.target.innerText - 1) * 12;
+    const projectEnd = (+e.target.innerText - 1) * 12 + 12
+    if (/^\d*$/.test(e.target.innerText)) {
+        featuredCon.innerHTML = projects.slice(projectStart, projectEnd).join('');
+    } else {
+        const sneakers = document.querySelectorAll('.sneaker');
+        const number = Math.floor(1 + (+sneakers[0].querySelector('.sneaker__preci').innerText) / 12) + 1;
+        featuredCon.innerHTML = projects.slice((number - 1) * 12, (number - 1) * 12 + 12).join('');
+    }
+
+})
